301dd100493e7bfa0a2c505013b18a8faabb7ac9
hankele2012 <han169@126.com>
2020-08-12 21:16:33 +0800

    添加session支持和route

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NetCore3x.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
